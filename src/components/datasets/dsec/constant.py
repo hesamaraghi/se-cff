ff62a655abfc0818e8c8e473e5434ba2a02f6293
hesamaraghi <hesam.araghi@gmail.com>
2025-02-25 22:27:24 +0100

    Update DATA_SPLIT in constant.py to use extract the file paths

--- src/components/datasets/dsec/constant.py
+++ src/components/datasets/dsec/constant.py
@@ -1,7 +1,16 @@
+from pathlib import Path  # Correct import
+
+data_dir = Path("data/NSEK")
+
+test_dir = data_dir / "test"
+train_dir = data_dir / "train"
+
 DATA_SPLIT = {
-    'train': ['train/interlaken_00_c'],
-    'validation': ['train/interlaken_00_c'],
-    'trainval': ['train/interlaken_00_c'],
-    'test': ['train/interlaken_00_c'],
+    'train': [k.parent.name + '/' + k.name for k in train_dir.iterdir() if k.name.startswith('K')],  # Fixed startswith
+    'validation': [],
+    'trainval': [],
+    'test': [k.parent.name + '/' + k.name  for k in test_dir.iterdir() if k.name.startswith('K')],  # Fixed startswith
     'none': [],
 }
+
+# print(DATA_SPLIT)
